85f15d00ecbc51d2c8b5d4433ae671a3c6ec0d9a
Raymond <rayreid76@gmail.com>
2018-08-19 10:51:24 -0700

    Porfolio II

--- Portfolio/Controllers/PortfolioController.cs
+++ Portfolio/Controllers/PortfolioController.cs
@@ -4,14 +4,42 @@ namespace Portfolio.Controllers
 {
     public class PortfolioController : Controller
     {
-        
-
-        
         [HttpGet("")]
         
         public ViewResult Index()
         {
             return View();
+        }
+
+        [HttpGet("projectslink")]
+        public RedirectResult Redirecting()
+        {
+            return Redirect("Projects");
+        }
+        [HttpGet("Projects")]
+        
+        public ViewResult Projects()
+        {
+            return View();
+        }
+
+        [HttpGet("contactlink")]
+
+        public RedirectResult contatRedirect()
+        {
+            return Redirect("Contact");
+        }
+
+        [HttpGet("Contact")]
+        public ViewResult Contact()
+        {
+            return View();
+        }
+        [HttpGet("homelink")]
+
+        public RedirectResult homeRedirect()
+        {
+            return Redirect("/");
         }
     }
 }
