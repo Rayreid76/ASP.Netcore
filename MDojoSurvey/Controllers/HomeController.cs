6534116fb2c4b26a7668bbe298d5c612dfd33efb
Raymond <rayreid76@gmail.com>
2018-08-24 15:04:38 -0700

    Model class survey form

---
+++ MDojoSurvey/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MDojoSurvey
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost("survey")]
+        public IActionResult DojoSurvey(LangSurvey mysurvey)
+        {
+            return Json(mysurvey);
+        }
+    }
+}
---
+++ MDojoSurvey/Models/Survey.cs
@@ -0,0 +1,11 @@
+namespace MDojoSurvey
+{
+    //survey class the is pasted throw the controller.
+    public class LangSurvey
+    {
+        public string FullName { get; set;}
+        public string DojoLocation {get; set;}
+        public string Language {get; set;}
+        public string Comment {get; set;}
+    }
+}
