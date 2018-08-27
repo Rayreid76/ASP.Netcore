67f8e8c70b18e2f2347a8b0d3fa4cf3383094645
Raymond <rayreid76@gmail.com>
2018-08-27 09:29:16 -0700

    Test app

--- MDojoSurvey/Models/Survey.cs
+++ MDojoSurvey/Models/Survey.cs
@@ -1,11 +1,19 @@
+using System.ComponentModel.DataAnnotations;
 namespace MDojoSurvey
 {
     //survey class the is pasted throw the controller.
     public class LangSurvey
     {
+        [Required]
+        [MinLength(2)]
+        [MaxLength(15)]
         public string FullName { get; set;}
+        [Required]
         public string DojoLocation {get; set;}
+        [Required]
         public string Language {get; set;}
+        [MinLength(8)]
+        [MaxLength(30)]
         public string Comment {get; set;}
     }
 }
